6839bb7c6921368c13f9f03d3b485ec2dcd06162
eisame <eisa.alamri@gmail.com>
2023-04-24 00:56:27 +0400

    setup.py change

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding = "utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "IPYNMrenderer"
+REPO_NAME = "IPYNBRENDERER"
 AUTHOR_USER_NAME = "eisame"
-SRC_REPO = "IPYNBrenderer"
-AUTHOR_EMAIL = "___@GMAIL.COM"
+SRC_REPO = "IPYNBRENDERER"
+AUTHOR_EMAIL = "eisa.alamri@GMAIL.COM"
 
 
 setuptools.setup(
